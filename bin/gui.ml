1b809defe74d944a6b93892a1002b62e249716ef
Rhea <ra677@cornell.edu>
2025-11-17 11:44:00 -0500

    implemented game start functionality & switching between screens/rooms

--- bin/gui.ml
+++ bin/gui.ml
@@ -2,14 +2,22 @@ open Bogue
 module W = Widget
 module L = Layout
 
+(** type for state for objects that can be toggled *)
 type toggle_state =
   | Closed
   | Open
 
+(** type for whether user inputted right or wrong answer *)
 type answer_result = {
   is_correct : bool;
   message : string;
 }
+
+(** type for room state*)
+type game_state = 
+  | Intro1
+  | Intro2
+  | StartingRoom
 
 (*actual pop up for when the user toggles an image that has a hint*)
 let puzzle_popup message on_submit parent_layout () =
@@ -62,14 +70,23 @@ let toggle_image ?w ?h ?x ?y ?(noscale = false) ~closed_image ~open_image ~puzzl
 (* actual display logic  *)
 let () =
   let bg_w, bg_h = (1280, 720) in
-  let background =
-    L.resident ~w:bg_w ~h:bg_h
-      (W.image ~w:bg_w ~h:bg_h ~noscale:true "images/starting_room.jpg")
-  in
+  let current_state = ref Intro1 in
+  let intro_bg = W.image ~noscale:true "images/starting_room_blurry.png" in
+  let intro_bg_layout = L.resident ~w:bg_w ~h:bg_h intro_bg in
 
-  let main_layout = L.superpose ~w:bg_w ~h:bg_h [ background ] in
-  L.auto_scale main_layout;
-  L.disable_resize main_layout;
+  let beginning = W.image ~w:1500 ~h:1050 ~noscale:true "images/click_to_begin.png" in
+  let beginning_layout = L.resident ~x:250 ~y:100 beginning in
+  let screen1 = L.superpose ~w:bg_w ~h:bg_h [ intro_bg_layout; beginning_layout ] in
+
+  let instructions = W.image ~w:1500 ~h:1050 ~noscale:true "images/instructions.png" in
+  let instr_layout = L.resident ~x:250 ~y:100 instructions in
+  let screen2 = L.superpose ~w:bg_w ~h:bg_h [ intro_bg_layout; instr_layout ] in
+
+  let main_bg =
+    W.image ~w:bg_w ~h:bg_h ~noscale:true "images/starting_room.jpg"
+  in
+  let main_bg_layout = L.resident ~w:bg_w ~h:bg_h main_bg in
+  let screen3 = L.superpose ~w:bg_w ~h:bg_h [ main_bg_layout ] in
   let treasure_room, treasure_state =
     toggle_image ~x:800 ~y:470 ~w:325 ~h:163
       ~closed_image:"images/chest_closed.png"
@@ -81,8 +98,8 @@ let () =
         else
           { is_correct = false; message = "Wrong answer: " ^ answer }
       )
-      main_layout ()
-  in
+      screen3 ()
+    in
 
   let casket_room, casket_state =
     toggle_image ~x:370 ~y:240 ~w:570 ~h:300
@@ -95,8 +112,32 @@ let () =
         else
           { is_correct = false; message = "Wrong answer: " ^ answer }
       )
-      main_layout ()
+      screen3 ()
   in
 
-  L.set_rooms main_layout [ background; treasure_room; casket_room ];
-  main_layout |> Bogue.of_layout |> Bogue.run 
+  L.set_rooms screen3 [main_bg_layout; treasure_room; casket_room];
+
+  let main_layout = L.superpose ~w:bg_w ~h:bg_h [ screen1 ] in
+  L.auto_scale main_layout;
+  L.disable_resize main_layout;
+
+  let transition_to_intro2 _ _ _ =
+    current_state := Intro2;
+    L.set_rooms main_layout [ screen2 ]
+  in
+  let transition_to_starting_room _ _ _ =
+    current_state := StartingRoom;
+    L.set_rooms main_layout [ screen3 ]
+  in
+  
+  (* Connect click handlers to screens *)
+  W.connect_main beginning beginning (fun ev x y ->
+    if !current_state = Intro1 then transition_to_intro2 ev x y
+  ) Trigger.buttons_up 
+  |> W.add_connection beginning;
+
+  W.connect_main instructions instructions (fun ev x y ->
+    if !current_state = Intro2 then transition_to_starting_room ev x y
+  ) Trigger.buttons_up 
+  |> W.add_connection instructions;
+  main_layout |> Bogue.of_layout |> Bogue.run
