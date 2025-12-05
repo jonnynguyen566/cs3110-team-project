25dbe2e89676d3b58c7beee2472878da9610b5f1
Sharon Chen <zc522@cornell.edu>
2025-12-04 23:00:45 -0500

    Added comments for room and puzzle mli files

--- lib/puzzle.mli
+++ lib/puzzle.mli
@@ -10,30 +10,43 @@ type puzzle_type =
   | Math of string * int
   | Trivia of string * string
 
-type puzzle
 (** Puzzle definition *)
+type puzzle
 
+(**[puzzle_id] is an accessor that returns the unique integer ID associated with [p]*)
 val puzzle_id : puzzle -> int
+(**[status] is an accessor that returns the current access status of [p]*)
 val status : puzzle -> puzzle_status
+(**[success_msg] is an accessor that returns the success message of [p]*)
 val success_msg : puzzle -> string
+(**[puzzle_type] is an accessor that returns the puzzle type of [p]*)
 val puzzle_type : puzzle -> puzzle_type
 
+(**[make] creates a new puzzle with the given parameters
+    - [id]:          globally unique puzzle ID  
+    - [puzzle_type]: the puzzle’s question and answer  
+    - [deps]:        list of puzzle IDs that must be solved before this one can unlock  
+    - [success_msg]: text displayed after solving the puzzle  *)
 val make :
   id:int ->
   puzzle_type:puzzle_type ->
   deps:int list ->
   success_msg:string ->
   puzzle
-(** Create a puzzle *)
 
+(** [check_answer] returns [true] if the player's input is the correct answer that solves puzzle [p], or [false] otherwise. *)
 val check_answer : puzzle -> string -> bool
-(** Try to solve a puzzle by checking an answer *)
 
+(** [mark_solved] Mark a puzzle as solved. Mutates [p] to have status [Solved] and does not perform dependency checks*)
 val mark_solved : puzzle -> unit
-(** Mark a puzzle as solved *)
 
+(** [try_unlock solved_ids p] unlocks puzzle [p] if all its dependency puzzle IDs
+    are present in [solved_ids].
+    - If all dependencies are satisfied, [p] becomes [Unlocked].
+    - If [p] is already [Unlocked] or [Solved], it remains unchanged.
+    - If dependencies are missing, [p] stays [Locked]. *)
 val try_unlock : int list -> puzzle -> unit
-(** Unlock puzzle if dependencies are solved *)
 
+(** [set_status p s] forcefully sets the status of puzzle [p] to [s].*)
 val set_status : puzzle -> puzzle_status -> unit
 (** Set the status of a puzzle *)
--- lib/room.mli
+++ lib/room.mli
@@ -1,7 +1,9 @@
+(** Abstract room status, accessibility of the room *)
 type room_status =
   | Inaccessible
   | Accessible
 
+(** Abstract type representing a room in the escape game. *)
 type room = {
   id : int;
   description : string;
@@ -11,11 +13,21 @@ type room = {
   intro_msg : string
 }
 
+(** [room_id] is an accessor that returns the unique ID of room [r]. *)
 val room_id : room -> int
+(** [status] is an accessor that returns the current status of room [r]. *)
 val status : room -> room_status
+(** [puzzles] is an accessor that returns the list of puzzles in room [r]. *)
 val puzzles : room -> Puzzle.puzzle list
+(** [intro_message] is an accessor that returns the introductory message of room [r]. *)
 val intro_message : room -> string
 
+(** [make] creates a new room with the given parameters
+    - [id]:          globally unique room ID  
+    - [description]: textual description of the room  
+    - [puzzles]:     list of puzzles contained in the room  
+    - [room_deps]:   list of room IDs that must be fulfilled before this one can unlock  
+    - [intro_msg]:   text displayed upon entering the room for the first time  *)
 val make :
   id:int ->
   description:string ->
@@ -23,9 +35,15 @@ val make :
   room_deps:int list ->
   intro_msg:string ->
   room
-(** Create a new room *)
+
+(** [room_fulfilled r] returns [true] if all puzzles in room [r] are solved. *)
 val room_fulfilled : room -> bool
+(** [try_unlock r ~solved_puzzles] unlocks room [r] by setting its status to
+    [Accessible] if all of its puzzle dependencies appear in [solved_puzzles]. *)
 val try_unlock : room -> solved_puzzles:int list -> unit
-(** Unlock room if dependencies are solved *)
+(** [is_accessible r] returns [true] if room [r] is currently in the [Accessible] state. *)
 val is_accessible : room -> bool
+(** [attempt_enter r ~solved_puzzles] attempts to enter room [r].
+    Returns [true] if the room becomes accessible (either already accessible
+    or successfully unlocked by checking dependencies), and [false] otherwise. *)
 val attempt_enter : room -> solved_puzzles:int list -> bool
